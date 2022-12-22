caf726d81de9725e1ff03caa996210a9dbe6e421
Atharva Shetty <94366989+Atharva-Shetty@users.noreply.github.com>
2022-12-22 15:18:27 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,104 @@
+
+from flask import Flask, render_template, request
+import bs4 as bs
+import urllib.request
+from recommender import *
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    suggestions = get_suggestions()
+    return render_template('home.html',suggestions=suggestions)
+
+@app.route("/similarity",methods=["POST"])
+def similarity():
+    movie = request.form['name']
+    recommended_movie = recommend_movie(movie)
+    
+    if isinstance(recommended_movie, str):
+        return recommended_movie
+    else:
+        return "-".join(recommended_movie)
+
+
+@app.route("/recommend",methods=["POST"])
+def recommend():
+
+    title = request.form['title']
+    
+    
+    cast_ids = request.form['cast_ids']
+    cast_names = request.form['cast_names']
+    cast_bios = request.form['cast_bios']
+    cast_places = request.form['cast_places']
+    cast_profiles = request.form['cast_profiles']
+    cast_chars = request.form['cast_chars']
+    cast_bdays = request.form['cast_bdays']
+    
+    
+    
+    imdb_id = request.form['imdb_id']
+    poster = request.form['poster']
+    genres = request.form['genres']
+    overview = request.form['overview']
+    release_date = request.form['release_date']
+    runtime = request.form['runtime']
+    status = request.form['status']
+    vote_average = request.form['rating']
+    vote_count = request.form['vote_count']
+    
+    
+    
+    rec_movies = request.form['rec_movies']
+    rec_posters = request.form['rec_posters']
+
+
+    suggestions = get_suggestions()
+
+    rec_movies = list_converter(rec_movies)
+    cast_chars = list_converter(cast_chars)
+    cast_profiles = list_converter(cast_profiles)
+    cast_bdays = list_converter(cast_bdays)    
+    rec_posters = list_converter(rec_posters)
+    cast_names = list_converter(cast_names)
+    cast_bios = list_converter(cast_bios)
+    cast_places = list_converter(cast_places)
+    
+    cast_ids = cast_ids.split(',')
+    cast_ids[0] = cast_ids[0].replace("[","")
+    cast_ids[-1] = cast_ids[-1].replace("]","")
+    
+    for i in range(len(cast_bios)):
+        cast_bios[i] = cast_bios[i].replace(r'\n', '\n').replace(r'\"','\"')
+    
+    movie_cards = {rec_posters[i]: rec_movies[i] for i in range(len(rec_posters))}
+
+    casts = {cast_names[i]:[cast_ids[i], cast_chars[i], cast_profiles[i]] for i in range(len(cast_profiles))}
+
+    cast_details = {cast_names[i]:[cast_ids[i], cast_profiles[i], cast_bdays[i], cast_places[i], cast_bios[i]] for i in range(len(cast_places))}
+
+    sauce = urllib.request.urlopen('https://www.imdb.com/title/{}/reviews?ref_=tt_ov_rt'.format(imdb_id)).read()
+    soup = bs.BeautifulSoup(sauce,'lxml')
+    soup_result = soup.find_all("div",{"class":"text show-more__control"})
+
+    reviews_list = [] 
+    reviews_status = [] 
+    for reviews in soup_result:
+        if reviews.string:
+            reviews_list.append(reviews.string)
+            movie_review_list = np.array([reviews.string])
+            movie_vector = vectorizer.transform(movie_review_list)
+            pred = clf.predict(movie_vector)
+            reviews_status.append('Good' if pred else 'Bad')
+
+   
+    movie_reviews = {reviews_list[i]: reviews_status[i] for i in range(len(reviews_list))}     
+
+   
+    return render_template('recommend.html',title=title,poster=poster,overview=overview,vote_average=vote_average,
+        vote_count=vote_count,release_date=release_date,runtime=runtime,status=status,genres=genres,
+        movie_cards=movie_cards,reviews=movie_reviews,casts=casts,cast_details=cast_details)
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ recommender.py
@@ -0,0 +1,60 @@
+import numpy as np
+import pandas as pd
+
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+import pickle
+
+
+filename = 'nlp.pkl'
+clf = pickle.load(open(filename, 'rb'))
+vectorizer = pickle.load(open('transform.pkl','rb'))
+
+def list_converter(arr):
+    arr = arr.split('","')
+    arr[0] = arr[0].replace('["','')
+    arr[-1] = arr[-1].replace('"]','')
+    return arr
+
+
+def get_suggestions():
+    data = pd.read_csv('main_data.csv')
+    return list(data['movie_title'].str.capitalize())
+
+def create_similarity():
+    data = pd.read_csv('main_data.csv')
+
+    cv = CountVectorizer()
+    count_matrix = cv.fit_transform(data['comb'])
+
+    similarity = cosine_similarity(count_matrix)
+    return data,similarity
+
+def recommend_movie(movie_name):
+    movie_name = movie_name.lower()
+    try:
+        data.head()
+        similarity.shape
+    except:
+        data, similarity = create_similarity()
+    if movie_name not in data['movie_title'].unique():
+        return("The movie wasen't found in our dataset")
+    else:
+        i = data.loc[data['movie_title']==movie_name].index[0]
+        lst = list(enumerate(similarity[i]))
+        lst = sorted(lst, key = lambda x:x[1] ,reverse=True)
+        lst = lst[1:11]
+        l = []
+        for i in range(len(lst)):
+            a = lst[i][0]
+            l.append(data['movie_title'][a])
+        return l
+    
+
+
+
+
+
+
+
